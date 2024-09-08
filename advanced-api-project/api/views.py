0119572de89eacb04d06995276fb76bbf13c5cd1
Franklin Akinboboye <boyefranklin@gmail.com>
2024-09-08 10:07:34 -0700

    Set Up Generic Views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Week 13 - Task 1 - step 1 
+
+# api/views.py
+
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+# ListView: Retrieve all books
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow read for all, create for authenticated users
+
+# DetailView: Retrieve a single book by ID
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow read for all, update/delete for authenticated users
+
