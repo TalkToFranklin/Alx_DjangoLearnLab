78299826538a7ba87daececf552d00fff6c8afa6
Franklin Akinboboye <boyefranklin@gmail.com>
2024-09-14 13:18:13 -0700

    Submission for Implementing the Blog's User Authentication System

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -22,11 +22,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True)
-
-    def __str__(self):
-        return self.user.username
