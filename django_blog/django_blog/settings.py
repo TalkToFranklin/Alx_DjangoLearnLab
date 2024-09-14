a46c71f1b44e9b1b2ab0ae95f841ebdb3e91b03b
Franklin Akinboboye <boyefranklin@gmail.com>
2024-09-14 12:04:03 -0700

    Submission for Initial Setup and Project Configuration for a Django Blog after Configuring the Database for PostgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+# Week 14 -Task 0 - Step 2 - Configured the Database for PostgreSQL
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.sqlite3', 'django_blog'
+        'USER': 'yourusername',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
