ec87c68687aeeac8323e4e4ab8a8813c7f1fad28
Franklin Akinboboye <boyefranklin@gmail.com>
2024-09-14 13:14:55 -0700

    Submission for Implementing the Blog's User Authentication System

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -22,3 +22,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True)
+
+    def __str__(self):
+        return self.user.username
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,24 @@
 # blog/urls.py
 
 from django.urls import path
+from . import views
 from .views import register, user_login, user_logout, profile
+
+#prpl
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('profile/', profile, name='profile'),
+]
+
+
+#cg4
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
