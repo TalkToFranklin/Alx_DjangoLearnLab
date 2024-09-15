a09bce565223ff43f02f5509e9904d211db57658
Franklin Akinboboye <boyefranklin@gmail.com>
2024-09-15 07:40:49 -0700

    Submission for Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'), # Week 14 - Task 2 - Step 4 Define URL Patterns
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
