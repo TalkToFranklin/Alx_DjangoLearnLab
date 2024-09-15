898f4560bbd5a65e15b7b10d4c6c270cb9d67c86
Franklin Akinboboye <boyefranklin@gmail.com>
2024-09-15 09:22:03 -0700

    Submission for Adding Comment Functionality to Blog Posts (used CBV for step 3)

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -104,7 +104,62 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Post, Comment
 from .forms import CommentForm
+from django.urls import reverse_lazy # Week 14 - Task 3 - Step_3 Implement Comment Views using class-based views (CBVs) include "CommentCreateView", "CommentUpdateView", "CommentDeleteView" 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+
+
+# Week 14 - Task 3 - Step_3 Implementation of Class-based views (CBVs) include "CommentCreateView", "CommentUpdateView", "CommentDeleteView" 
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/add_comment.html'
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Your comment has been posted.')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.id})
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/edit_comment.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Your comment has been updated.')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.id})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/delete_comment.html'
+
+    def get_success_url(self):
+        messages.success(self.request, 'Your comment has been deleted.')
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.id})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+# Before CBV implementation
 
 @login_required
 def add_comment(request, post_id):
